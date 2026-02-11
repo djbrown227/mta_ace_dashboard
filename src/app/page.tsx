e65c6658929c51b7bce098bd897262f55e29c90d
“djbrown227” <djbrown227@gmail.com>
2026-02-11 10:50:23 -0500

    Ready for Vercel deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function DashboardPage() {
 
   useEffect(() => {
     // Show intro for 4 seconds on initial mount
-    const timer = setTimeout(() => setShowDashboard(true), 4000);
+    const timer = setTimeout(() => setShowDashboard(true), 5000);
     return () => clearTimeout(timer);
   }, []);
 
